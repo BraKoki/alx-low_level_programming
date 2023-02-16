41876e888fa2d2fa2e4c4ed0378ccf47e57c0687
Koketso <koketso2010@hotmail.com>
2023-02-16 12:32:06 -0800

    Task 4.Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a message regarding C
+ * Return: 0 (GoodGame)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
