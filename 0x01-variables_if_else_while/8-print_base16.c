5833c3321604947b04db6be20ad13a8e6c84a16c
Koketso <koketso2010@hotmail.com>
2023-02-18 09:46:03 -0800

    Task 9.Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0 (GoodGame)
+ */
+int main(void)
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+	(d < 10)
+{
+	putchar(d + '0');
+}
+while
+(c <= 'f')
+{
+	putchar(c);
+	c++;
+}
+putchar('\n');
+return (0);
+}
