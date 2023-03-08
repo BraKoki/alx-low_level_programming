aaf4b82223b830ae756434d9ff2df8da5b22b3ec
Koketso <koketso2010@hotmail.com>
2023-03-08 11:12:22 -0800

    Task 6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,9 +3,9 @@
 int actual_prime(int n, int i);
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number.
+ * is_prime_number - returns 1 if the input integer is a prime number
  * @n: number to evaluate
- * Return: 1 if n is a prime number, 0 is not
+ * Return: 1 if n a prime number, 0 if not
  */
 
 int is_prime_number(int n)
@@ -16,10 +16,10 @@ int is_prime_number(int n)
 }
 
 /**
- * actual_prime - calculates a prime number recursively.
+ * actual_prime - calculate if a number is prime
  * @n: number to evaluate
  * @i: iterator
- * Return 1 if n is a prime number, 0 if not
+ * Return: 1 if n is prime, 0 if not
  */
 
 int actual_prime(int n, int i)
